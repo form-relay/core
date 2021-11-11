b6832e681e32a629eaf73cfb6c228a508d43716a
Stephan Ude <ude@mediatis.de>
2021-11-11 18:19:35 +0100

    MEDO-563: fixed equals evaluation for multi value fields

--- src/ConfigurationResolver/Evaluation/EqualsEvaluation.php
+++ src/ConfigurationResolver/Evaluation/EqualsEvaluation.php
@@ -2,9 +2,16 @@
 
 namespace FormRelay\Core\ConfigurationResolver\Evaluation;
 
+use FormRelay\Core\Model\Form\MultiValueField;
+
 class EqualsEvaluation extends AbstractComparisonEvaluation
 {
     protected function evalValue($fieldValue, array $keysEvaluated = [])
+    {
+        return $this->compare($fieldValue, $this->resolveContent($this->configuration));
+    }
+
+    protected function evalMultiValue(MultiValueField $fieldValue, array $keysEvaluated = []): bool
     {
         return $this->compare($fieldValue, $this->resolveContent($this->configuration));
     }
