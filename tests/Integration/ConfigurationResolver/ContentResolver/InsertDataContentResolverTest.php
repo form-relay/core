b14f1577763cc71040d07a1c5d74cbbbff1418bd
Stephan Ude <ude@mediatis.de>
2022-04-27 10:54:58 +0200

    update tests

--- tests/Integration/ConfigurationResolver/ContentResolver/InsertDataContentResolverTest.php
+++ tests/Integration/ConfigurationResolver/ContentResolver/InsertDataContentResolverTest.php
@@ -5,12 +5,17 @@ namespace FormRelay\Core\Tests\Integration\ConfigurationResolver\ContentResolver
 use FormRelay\Core\ConfigurationResolver\ContentResolver\InsertDataContentResolver;
 use FormRelay\Core\Model\Form\MultiValueField;
 use FormRelay\Core\Model\Submission\SubmissionConfigurationInterface;
+use FormRelay\Core\Tests\Model\Form\StringField;
 
 /**
  * @covers InsertDataContentResolver
  */
 class InsertDataContentResolverTest extends AbstractContentResolverTest
 {
+    // TODO use AbstractModifierContentResolverTest as parent
+    //      though not all tests can be put into provider arrays
+    //      because the content resolver can collapse multi-values to strings
+
     public function insertDataProvider(): array
     {
         return [
@@ -53,7 +58,7 @@ class InsertDataContentResolverTest extends AbstractContentResolverTest
     }
 
     /** @test */
-    public function insertDataMultiValueOnly()
+    public function insertMultiValueIntoOnePlaceholderOnly()
     {
         $this->submissionData['field1'] = new MultiValueField([5, 7, 17]);
         $config = [
@@ -65,7 +70,7 @@ class InsertDataContentResolverTest extends AbstractContentResolverTest
     }
 
     /** @test */
-    public function insertDataContainsMultiValue()
+    public function insertMultiValueIntoPlaceholderWithContext()
     {
         $this->submissionData['field1'] = new MultiValueField([5, 7, 17]);
         $config = [
@@ -74,5 +79,93 @@ class InsertDataContentResolverTest extends AbstractContentResolverTest
         ];
         $result = $this->runResolverProcess($config);
         $this->assertEquals('field1: 5,7,17', $result);
+    }
+
+    /** @test */
+    public function insertDataIntoMultiValue()
+    {
+        $this->submissionData['field1'] = 'value1';
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => new MultiValueField(['{field1}']),
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertMultiValueEquals(new MultiValueField(['value1']), $result);
+    }
+
+    /** @test */
+    public function insertDataIntoNestedMultiValue()
+    {
+        $this->submissionData['field1'] = 'value1';
+        $this->submissionData['field2'] = 'value2';
+        $this->submissionData['field3'] = 'value3';
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => new MultiValueField([
+                '{field1}',
+                new MultiValueField(['{field2}','{field3}']),
+            ]),
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertMultiValueEquals(new MultiValueField([
+            'value1',
+            new MultiValueField(['value2','value3']),
+        ]), $result);
+    }
+
+    /** @test */
+    public function insertNonPrimitiveField()
+    {
+        $this->submissionData['field1'] = new StringField('value1');
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => '{field1}',
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertIsObject($result);
+        $this->assertInstanceOf(StringField::class, $result);
+        $this->assertEquals('value1', (string)$result);
+    }
+
+    /** @test */
+    public function insertNonPrimitiveFieldWithContext()
+    {
+        $this->submissionData['field1'] = new StringField('value1');
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'field1:{field1}',
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertIsString($result);
+        $this->assertEquals('field1:value1', $result);
+    }
+
+    /** @test */
+    public function insertNonPrimitiveFieldIntoMultiValue()
+    {
+        $this->submissionData['field1'] = new StringField('value1');
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => new MultiValueField(['{field1}']),
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertMultiValueEquals(new MultiValueField(['value1']), $result);
+        $this->assertIsObject($result['0']);
+        $this->assertInstanceOf(StringField::class, $result['0']);
+        $this->assertEquals('value1', (string)$result['0']);
+    }
+
+    /** @test */
+    public function insertNonPrimitiveFieldIntoMultiValueWithContext()
+    {
+        $this->submissionData['field1'] = new StringField('value1');
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => new MultiValueField(['field1:{field1}']),
+            'insertData' => true,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertMultiValueEquals(new MultiValueField(['field1:value1']), $result);
+        $this->assertIsString($result['0']);
+        $this->assertEquals('field1:value1', $result['0']);
     }
 }
