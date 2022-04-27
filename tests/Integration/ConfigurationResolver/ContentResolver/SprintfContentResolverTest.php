3b731abef41f9b891dd7a002edf6f3e5449a8e97
Stephan Ude <ude@mediatis.de>
2022-04-27 10:22:00 +0200

    test updates

--- tests/Integration/ConfigurationResolver/ContentResolver/SprintfContentResolverTest.php
+++ tests/Integration/ConfigurationResolver/ContentResolver/SprintfContentResolverTest.php
@@ -3,7 +3,6 @@
 namespace FormRelay\Core\Tests\Integration\ConfigurationResolver\ContentResolver;
 
 use FormRelay\Core\ConfigurationResolver\ContentResolver\SprintfContentResolver;
-use FormRelay\Core\Model\Form\MultiValueField;
 use FormRelay\Core\Model\Submission\SubmissionConfigurationInterface;
 
 /**
@@ -11,43 +10,192 @@ use FormRelay\Core\Model\Submission\SubmissionConfigurationInterface;
  */
 class SprintfContentResolverTest extends AbstractContentResolverTest
 {
-    public function sprintfProvider(): array
-    {
-        return [
-            // value, format, expected
-            [null, 'format1', null],
-            [null, '', null],
+    // NOTE: the parent class AbstractModifierContentResolverTest can't be used here
+    //       because this content resolver handles multi-value fields differently
 
-            ['value1', '', 'value1'],
-            ['value1', 'format1', 'format1'],
-            ['value1', '%s', 'value1'],
-            ['value1', 'format1:%s', 'format1:value1'],
-
-            ['1.2', '%01.2f', '1.20'],
-            [1.2, '%01.2f', '1.20'],
-            [34.567, '%01.2f', '34.57'],
-
-            [new MultiValueField(['value1', 'value2']), 'format1', 'format1'],
-            [new MultiValueField(['value1', 'value2']), '%s', 'value1'],
-            [new MultiValueField(['value1', 'value2']), '%s:%s', 'value1:value2'],
-            [new MultiValueField([1.2, 34.567]), '%01.2f - %01.2f', '1.20 - 34.57'],
-        ];
-    }
-
-    /**
-     * @param $value
-     * @param $format
-     * @param $expected
-     * @dataProvider sprintfProvider
-     * @test
-     */
-    public function sprintf($value, $format, $expected)
+    /** @test */
+    public function nullIsNotProcessed()
     {
         $config = [
-            SubmissionConfigurationInterface::KEY_SELF => $value,
-            'sprintf' => $format,
+            SubmissionConfigurationInterface::KEY_SELF => null,
+            'sprintf' => 'format1',
         ];
         $result = $this->runResolverProcess($config);
-        $this->assertEquals($expected, $result);
+        $this->assertNull($result);
+    }
+
+    /** @test */
+    public function emptyFormatLeadsToEmptyResult()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => '',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('', $result);
+    }
+
+    /** @test */
+    public function boolFalseFormatDoesNotGetProcessed()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => false,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value1', $result);
+    }
+
+    /** @test */
+    public function staticFormatIsPassedThrough()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => 'format1',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('format1', $result);
+    }
+
+    /** @test */
+    public function valueIsUsedAsPlaceholder()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => '%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value1', $result);
+    }
+
+    /** @test */
+    public function valueIsUsedAsPlaceholderInComplexFormat()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => 'format1:%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('format1:value1', $result);
+    }
+
+    /** @test */
+    public function floatFormatting()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 1.2,
+            'sprintf' => '%01.2f',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('1.20', $result);
+    }
+
+    /** @test */
+    public function multiValueIsUsedForMultiplePlaceholders()
+    {
+        $config = [
+            'multiValue' => ['value1', 'value2'],
+            'sprintf' => '%s:%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value1:value2', $result);
+    }
+
+    /** @test */
+    public function multiValueUsedAsPlaceholdersWithMoreValuesThanPlaceholders()
+    {
+        $config = [
+            'multiValue' => ['value1', 'value2'],
+            'sprintf' => '%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value1', $result);
+    }
+
+    /** @test */
+    public function multiValueIsJoinedAndUsedAsOnePlaceholder()
+    {
+        $config = [
+            'multiValue' => ['value1', 'value2'],
+            'join' => ['glue' => ','],
+            'sprintf' => '%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value1,value2', $result);
+    }
+
+    /** @test */
+    public function multiValueUsedForFloatingpointPlaceholders()
+    {
+        $config = [
+            'multiValue' => [1.2, 34.567],
+            'sprintf' => '%01.2f - %01.2f',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('1.20 - 34.57', $result);
+    }
+
+    /** @test */
+    public function nestedMultiValuesAreFlattenOutInPlaceholders()
+    {
+        $config = [
+            'multiValue' => [
+                ['multiValue' => ['a', 'b']],
+                'c',
+                ['multiValue' => ['d']],
+                'e',
+                ['multiValue' => []],
+                'f'
+            ],
+            'sprintf' => '%s:%s:%s:%s:%s:%s',
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('a,b:c:d:e::f', $result);
+    }
+
+    /** @test */
+    public function concatenatedFormat()
+    {
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => [
+                'glue' => ':',
+                'format1',
+                '%s',
+            ],
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('format1:value1', $result);
+    }
+
+    /** @test */
+    public function conditionalFormatWithTwoOptions()
+    {
+        $this->submissionData['field1'] = 'value1';
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value1',
+            'sprintf' => ['if' => [
+                'field1' => 'value1',
+                'then' => 'formatThen:%s',
+                'else' => 'formatElse:%s',
+            ]],
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('formatThen:value1', $result);
+    }
+
+    /** @test */
+    public function conditionalFormatWithNullOptionDoesNotGetProcessed()
+    {
+        $this->submissionData['field1'] = 'value1';
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => 'value2',
+            'sprintf' => ['if' => [
+                'field1' => 'value3',
+                'then' => 'formatThen:%s',
+            ]],
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals('value2', $result);
     }
 }
