be4b4f4747b513bbc745b394336f180bc4a1517b
Stephan Ude <ude@mediatis.de>
2022-04-27 10:44:11 +0200

    test updates

--- tests/Integration/ConfigurationResolver/ContentResolver/AbstractModifierContentResolverTest.php
+++ tests/Integration/ConfigurationResolver/ContentResolver/AbstractModifierContentResolverTest.php
@@ -13,11 +13,63 @@ abstract class AbstractModifierContentResolverTest extends AbstractContentResolv
 
     abstract public function modifyMultiValueProvider(): array;
 
-    protected function runModify($value, $expected, $enabled)
+    public function disabledConfigurationProvider(): array
+    {
+        return [
+            [false],
+            [[]],
+            [new MultiValueField([])],
+            [['if' => [
+                'field1' => 'value999', // => false
+                'then' => true,
+                'else' => false,
+            ]]],
+            [['if' => [
+                'field1' => 'value999', // => false
+                'then' => true,
+            ]]],
+        ];
+    }
+
+    /**
+     * A standard value that the modifier can run on.
+     * Used for testing a disabled modifier where the value should never change.
+     * @return mixed
+     */
+    protected function getStandardValue()
+    {
+        return 'standardValue';
+    }
+
+    /**
+     * @param $disabledConfig
+     * @dataProvider disabledConfigurationProvider
+     * @test
+     */
+    public function modifierDisabledDoesNotChangeValue($disabledConfig)
+    {
+        $this->submissionData['field1'] = 'value1';
+        $value = $this->getStandardValue();
+        $config = [
+            SubmissionConfigurationInterface::KEY_SELF => $value,
+            static::KEYWORD => $disabledConfig,
+        ];
+        $result = $this->runResolverProcess($config);
+        $this->assertEquals($value, $result);
+    }
+
+    /**
+     * @param $value
+     * @param $expected
+     * @param $modifierConfig
+     * @dataProvider modifyProvider
+     * @test
+     */
+    public function modify($value, $expected, $modifierConfig = true)
     {
         $config = [
             SubmissionConfigurationInterface::KEY_SELF => $value,
-            static::KEYWORD => $enabled,
+            static::KEYWORD => $modifierConfig,
         ];
         $result = $this->runResolverProcess($config);
         if ($expected === null) {
@@ -30,20 +82,15 @@ abstract class AbstractModifierContentResolverTest extends AbstractContentResolv
     /**
      * @param $value
      * @param $expected
-     * @dataProvider modifyProvider
+     * @param $modifierConfig
+     * @dataProvider modifyMultiValueProvider
      * @test
      */
-    public function modify($value, $expected)
-    {
-        $this->runModify($value, $expected, true);
-        $this->runModify($value, $value, false);
-    }
-
-    protected function runModifyMultiValue($value, $expected, $enabled)
+    public function modifyMultiValue($value, $expected, $modifierConfig = true)
     {
         $config = [
             'multiValue' => $value,
-            static::KEYWORD => $enabled,
+            static::KEYWORD => $modifierConfig,
         ];
         $result = $this->runResolverProcess($config);
         if (empty($expected)) {
@@ -53,43 +100,25 @@ abstract class AbstractModifierContentResolverTest extends AbstractContentResolv
         }
     }
 
-
     /**
      * @param $value
      * @param $expected
+     * @param $modifierConfig
      * @dataProvider modifyMultiValueProvider
      * @test
      */
-    public function modifyMultiValue($value, $expected)
-    {
-        $this->runModifyMultiValue($value, $expected, true);
-        $this->runModifyMultiValue($value, $value, false);
-    }
-
-    protected function runModifyNestedMultiValue($value, $expected, $enabled)
+    protected function modifyNestedMultiValue($value, $expected, $modifierConfig = true)
     {
         $config = [
             'multiValue' => [
                 ['multiValue' => $value],
             ],
-            static::KEYWORD => $enabled,
+            static::KEYWORD => $modifierConfig,
         ];
         /** @var MultiValueField $result */
         $result = $this->runResolverProcess($config);
         $this->assertMultiValue($result);
         $result = $result->toArray()[0];
         $this->assertMultiValueEquals($expected, $result);
-    }
-
-    /**
-     * @param $value
-     * @param $expected
-     * @dataProvider modifyMultiValueProvider
-     * @test
-     */
-    public function modifyNestedMultiValue($value, $expected)
-    {
-        $this->runModifyNestedMultiValue($value, $expected, true);
-        $this->runModifyNestedMultiValue($value, $value, false);
     }
 }
