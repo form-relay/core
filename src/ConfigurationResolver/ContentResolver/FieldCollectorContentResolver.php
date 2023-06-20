9da8e2bff25a7552c131971f46ac80e50b5fec45
Stephan Ude <ude@mediatis.de>
2023-06-20 13:14:21 +0200

    [FEATURE] field collector - include option

--- src/ConfigurationResolver/ContentResolver/FieldCollectorContentResolver.php
+++ src/ConfigurationResolver/ContentResolver/FieldCollectorContentResolver.php
@@ -9,6 +9,9 @@ class FieldCollectorContentResolver extends ContentResolver
     const KEY_EXCLUDE = 'exclude';
     const DEFAULT_EXCLUDE = '';
 
+    const KEY_INCLUDE = 'include';
+    const DEFAULT_INCLUDE = '';
+
     const KEY_IGNORE_IF_EMPTY = 'ignoreIfEmpty';
     const DEFAULT_IGNORE_IF_EMPTY = true;
 
@@ -26,23 +29,27 @@ class FieldCollectorContentResolver extends ContentResolver
     public function build()
     {
         $exclude = $this->resolveContent($this->getConfig(static::KEY_EXCLUDE));
+        $include = $this->resolveContent($this->getConfig(static::KEY_INCLUDE));
         $ignoreIfEmpty = $this->evaluate($this->getConfig(static::KEY_IGNORE_IF_EMPTY));
         $unprocessedOnly = $this->evaluate($this->getConfig(static::KEY_UNPROCESSED_ONLY));
         $template = $this->resolveContent($this->getConfig(static::KEY_TEMPLATE));
 
         $excludedFields = GeneralUtility::castValueToArray($exclude);
+        $includeFields = GeneralUtility::castValueToArray($include);
         $template = GeneralUtility::parseSeparatorString($template);
 
         $result = '';
         foreach ($this->context->getData() as $key => $value) {
-            if (in_array($key, $excludedFields)) {
-                continue;
-            }
-            if ($ignoreIfEmpty && GeneralUtility::isEmpty($value)) {
-                continue;
-            }
-            if ($unprocessedOnly && $this->context->getFieldTracker()->hasBeenProcessed($key)) {
-                continue;
+            if (empty($includedFields) || !in_array($key, $includedFields)) {
+                if (in_array($key, $excludedFields)) {
+                    continue;
+                }
+                if ($ignoreIfEmpty && GeneralUtility::isEmpty($value)) {
+                    continue;
+                }
+                if ($unprocessedOnly && $this->context->getFieldTracker()->hasBeenProcessed($key)) {
+                    continue;
+                }
             }
             $part = $template;
             $part = str_replace('{key}', $key, $part);
@@ -56,6 +63,7 @@ class FieldCollectorContentResolver extends ContentResolver
     {
         return parent::getDefaultConfiguration() + [
             static::KEY_EXCLUDE => static::DEFAULT_EXCLUDE,
+            static::KEY_INCLUDE => static::DEFAULT_INCLUDE,
             static::KEY_IGNORE_IF_EMPTY => static::DEFAULT_IGNORE_IF_EMPTY,
             static::KEY_UNPROCESSED_ONLY => static::DEFAULT_UNPROCESSED_ONLY,
             static::KEY_TEMPLATE => static::DEFAULT_TEMPLATE,
